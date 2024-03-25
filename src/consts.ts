88726370d7cd45f7498a1a9904ae155cd37995f6
Arillaxe <bekirshabut66@gmail.com>
2024-03-25 03:45:29 +0300

    Initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,115 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Arillaxe.dev";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const CONTACTS = [
+  {
+    label: "Github link",
+    icon: "github",
+    link: "https://github.com/Arillaxe",
+  },
+  {
+    label: "Linkedin link",
+    icon: "linkedin",
+    link: "https://linkedin.com/in/bekir-shabutov",
+  },
+  {
+    label: "Telegram link",
+    icon: "telegram",
+    link: "https://t.me/Arillaxe",
+  },
+  {
+    label: "Steam link",
+    icon: "steam",
+    link: "https://steamcommunity.com/id/Arillaxe",
+  },
+];
+
+export const SKILLS = [
+  {
+    title: "Front-end",
+    skills: [
+      {
+        label: "React",
+        icon: "react",
+      },
+      {
+        label: "Next.js",
+        icon: "nextjs",
+      },
+      {
+        label: "Vue",
+        icon: "vuejs",
+      },
+      {
+        label: "Redux",
+        icon: "redux",
+      },
+      {
+        label: "Sass",
+        icon: "sass",
+      },
+    ],
+  },
+  {
+    title: "Back-end",
+    skills: [
+      {
+        label: "Node.js",
+        icon: "nodejs",
+      },
+      {
+        label: "Nest.js",
+        icon: "nestjs",
+      },
+      {
+        label: "Go",
+        icon: "golang",
+      },
+    ],
+  },
+  {
+    title: "Database",
+    skills: [
+      {
+        label: "PostgreSQL",
+        icon: "psql",
+      },
+      {
+        label: "Mongo DB",
+        icon: "mongodb",
+      },
+      {
+        label: "Redis",
+        icon: "redis",
+      },
+    ],
+  },
+  {
+    title: "Misc",
+    skills: [
+      {
+        label: "Linux",
+        icon: "linux",
+      },
+      {
+        label: "Docker",
+        icon: "docker",
+      },
+      {
+        label: "Nginx",
+        icon: "nginx",
+      },
+      {
+        label: "C++",
+        icon: "cpp",
+      },
+      {
+        label: "Jest",
+        icon: "jest",
+      },
+    ],
+  },
+];
