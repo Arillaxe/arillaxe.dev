65fdc308f8fc5484eca03f87cc0f1156da65bbdc
Arillaxe <arillaxe@gmail.com>
2024-03-31 05:02:02 +0300

    Configure meta tags, add 404 page, add favicon

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Arillaxe.dev";
-export const SITE_DESCRIPTION = "Arillaxe's homepage";
+export const SITE_DESCRIPTION = "Arillaxe's webpage";
 
 export const CONTACTS = [
   {
